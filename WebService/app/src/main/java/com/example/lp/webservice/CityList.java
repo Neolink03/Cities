d62cdae31d05f009c4618050db4caa3a70f8fbcc
lp <lp>
2016-12-02 17:18:26 +0100

    Add list of cities

---
+++ WebService/app/src/main/java/com/example/lp/webservice/CityList.java
@@ -0,0 +1,16 @@
+package com.example.lp.webservice;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lp on 02/12/2016.
+ */
+
+public class CityList extends ArrayList<JSONObject> {
+
+
+}
