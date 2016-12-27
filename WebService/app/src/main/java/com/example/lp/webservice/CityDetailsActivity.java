bc61daaf47767f9ac4cf5e2beb74e8327e272598
Jonathan <Neolink03>
2016-12-27 15:18:18 +0100

    Begin add update function city
    TODO : add update function in rest api

--- WebService/app/src/main/java/com/example/lp/webservice/CityDetailsActivity.java
+++ WebService/app/src/main/java/com/example/lp/webservice/CityDetailsActivity.java
@@ -4,16 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CityDetailsActivity extends AppCompatActivity {
 
@@ -35,6 +41,7 @@ public class CityDetailsActivity extends AppCompatActivity {
             this.cityName = fetchCityNameFromExtras();
             displayCityNameTitle();
             displayCityDetails();
+            //update();
         }
         else {
             ToastMessage.noNetworkConnection(this);
@@ -111,6 +118,50 @@ public class CityDetailsActivity extends AppCompatActivity {
         this.cityDetailTextViews.add( (TextView) findViewById(R.id.latitudeLabelTextView) );
         this.cityDetailTextViews.add( (TextView) findViewById(R.id.longitudeLabelTextView) );
         this.cityDetailTextViews.add( (TextView) findViewById(R.id.remotenessLabelTextView) );
+    }
+
+    public void update() {
+        String tag_json_obj = "json_obj_req";
+
+        String ipServer = "http://10.0.2.1";
+        String url = ipServer + "/villes/" + cityName;
+
+        JsonObjectRequest request = new JsonObjectRequest
+                (Request.Method.PUT, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject cityListJSONArrayResponse) {
+                        System.out.println("ok");
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        ToastMessage.displayUnexpectedResponse(getApplicationContext());
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("nbr_habitants", "42");
+
+                return params;
+            }
+
+                /**
+                 * Passing some request headers
+                 * */
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+        };
+
+        RequestQueue.getInstance(this).addToRequestQueue(request);
+
     }
 
     @Override
