8210e25da7515163d086881ff348cca780ea56bc
Jonathan <Neolink03>
2016-12-27 17:30:37 +0100

    update city function in CityEditActivity
    remove function from CityDetailsActivity

--- WebService/app/src/main/java/com/example/lp/webservice/CityDetailsActivity.java
+++ WebService/app/src/main/java/com/example/lp/webservice/CityDetailsActivity.java
@@ -130,50 +130,6 @@ public class CityDetailsActivity extends AppCompatActivity {
         this.cityDetailTextViews.add( (TextView) findViewById(R.id.inhabitantNumberTextView) );
     }
 
-    public void update() {
-        String tag_json_obj = "json_obj_req";
-
-        String ipServer = "http://10.0.2.1";
-        String url = ipServer + "/villes/" + cityName;
-
-        JsonObjectRequest request = new JsonObjectRequest
-                (Request.Method.PUT, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject cityListJSONArrayResponse) {
-                        System.out.println("ok");
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        ToastMessage.displayUnexpectedServerResponse(getApplicationContext());
-                    }
-                }) {
-
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("nbr_habitants", "42");
-
-                return params;
-            }
-
-                /**
-                 * Passing some request headers
-                 * */
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json");
-                return headers;
-            }
-
-        };
-
-        RequestQueue.getInstance(this).addToRequestQueue(request);
-
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
