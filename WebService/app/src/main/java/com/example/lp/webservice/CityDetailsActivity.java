26cda1874370ddf1da966e615080e54537d42923
Jonathan <Neolink03>
2017-01-08 19:02:18 +0100

    Add menu to delete / modify a city

--- WebService/app/src/main/java/com/example/lp/webservice/CityDetailsActivity.java
+++ WebService/app/src/main/java/com/example/lp/webservice/CityDetailsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -44,6 +46,13 @@ public class CityDetailsActivity extends AppCompatActivity {
         else {
             ToastMessage.noNetworkConnection(this);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     public void deleteCity() {
