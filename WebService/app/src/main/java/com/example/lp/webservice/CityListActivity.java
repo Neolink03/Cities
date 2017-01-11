dc621e2393d9396917aba1e22180afabf01a8f80
Neolink03 <link.03@hotmail.fr>
2017-01-11 16:33:51 +0100

    Add create city button list activity

--- WebService/app/src/main/java/com/example/lp/webservice/CityListActivity.java
+++ WebService/app/src/main/java/com/example/lp/webservice/CityListActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -44,6 +46,13 @@ public class CityListActivity extends AppCompatActivity {
         cityList = (ListView) findViewById(R.id.cityListListView);
         citySearchBar = (EditText) findViewById(R.id.citySearchBarTextField);
         createCity(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.city_list_menu, menu);
+        return true;
     }
 
     @Override
